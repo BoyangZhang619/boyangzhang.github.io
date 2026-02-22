907023171fd6934c0cd10a76900f3927a1f723c4
ZhangBY619 <boyangzhang246@gmail.com>
2026-02-22 16:04:28 +0800

    fix: 修复 favicon 更新逻辑，确保图标类型为 PNG 并添加缓存破坏参数

--- src/main.ts
+++ src/main.ts
@@ -7,16 +7,16 @@ createApp(App).mount('#app')
 const pageIconAddresses: string[] = ['/iconWhite.png', '/iconBlack.png'];
 const updateFavicon = () => {
   const hour = new Date().getHours();
-  const iconAddress = hour >= 7 && hour < 19 ? pageIconAddresses[0] : pageIconAddresses[1];
-  if (!iconAddress) return;
   let linkElement = document.querySelector("link[rel='icon']") as HTMLLinkElement | null;
   if (!linkElement) {
     linkElement = document.createElement('link');
     linkElement.rel = 'icon';
     document.head.appendChild(linkElement);
   }
-  linkElement.type = 'image/jpeg';
-  linkElement.href = `${iconAddress}?v=${Date.now()}`;
+  linkElement.type = 'image/png';
+  const iconAddress = hour >= 7 && hour < 19 ? pageIconAddresses[0] : pageIconAddresses[1];
+  if (!iconAddress) return;
+  linkElement.href = iconAddress;
 };
 updateFavicon();
 setInterval(updateFavicon, 10 * 1000);
